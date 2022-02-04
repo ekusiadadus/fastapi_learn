4f334d6ab990026d6f38700bdf241ca7cdf10b26
ekusiadadus <ekusiadadus@gmail.com>
2022-02-04 08:16:32 +0000

    FIX: pycache

--- main.py
+++ main.py
@@ -1,27 +1,50 @@
-from tokenize import String
+from random import randrange
+from typing import Optional
+
 from fastapi import FastAPI
-from fastapi.params import Body
 from pydantic import BaseModel
 
 app = FastAPI()
 
+
 class Post(BaseModel):
-  title: str
-  content: str
-  published: bool = True
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, {"title": "favorites foods", "content": "I like pizza", "id": 2}]
+
+
+def find_post(id: int):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
+
 
 # request Get method url: "/"
 
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods
-@app.get("/") 
+@app.get("/")
 async def root():
-  return {"message": "Hello World"}
+    return {"message": "Hello World"}
+
 
 @app.get("/posts")
 def get_posts():
-  return {"data": "This is your posts"}
+    return {"data": my_posts}
+
 
 @app.post("/createposts")
-def create_posts(new_post: Post):
-  return {"data": new_post}
-# title str, content str
+def create_posts(post: Post):
+    post_dict = post.dict()
+    post_dict["id"] = randrange(0, 100000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
+
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    post = find_post(id)
+    return {"post_detail": post}
