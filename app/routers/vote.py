d6bb786d16660032ff56e255ca1b59cec1ed5a5a
ekusiadadus <ekusiadadus@gmail.com>
2022-02-08 15:03:55 +0000

    add: vote methods

---
+++ app/routers/vote.py
@@ -0,0 +1,27 @@
+from sys import prefix
+
+from fastapi import APIRouter, Depends, FastAPI, HTTPException, Response, status
+from sqlalchemy.orm import Session
+
+from .. import database, models, oauth2, schemas
+
+router = APIRouter(prefix="/vote", tags=["Vote"])
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+def vote(vote: schemas.Vote, db: Session = Depends(database.get_db), current_user: models.User = Depends(oauth2.get_current_user)):
+    vote_query = db.query(models.Vote).filter(models.Vote.post_id == vote.post_id, models.Vote.user_id == current_user.id)
+    found_vote = vote_query.first()
+    if vote.dir == 1:
+        if found_vote:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"user {current_user.id} has already voted on post {vote.post_id}")
+        new_vote = models.Vote(post_id=vote.post_id, user_id=current_user.id)
+        db.add(new_vote)
+        db.commit()
+        return {"message": "successfully added vote"}
+    else:
+        if not found_vote:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Voute does not exist")
+        vote_query.delete(sychronize_session=False)
+        db.commit()
+        return {"message": "successfully deleted vote"}
