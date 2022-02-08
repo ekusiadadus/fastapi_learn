5d480c7f6fcfca4fb9a5740acfc29fc14c8d6cde
ekusiadadus <ekusiadadus@gmail.com>
2022-02-08 08:30:05 +0000

    Fix: Refactor Hard Coding

--- app/database.py
+++ app/database.py
@@ -6,7 +6,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://fastapi:fastapi@127.0.0.1:5432/fastapi"
+from .config import settings
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
