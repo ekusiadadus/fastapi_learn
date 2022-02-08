80f0fb37c46f5c687a0c4a7121ce7c62af38a2ad
ekusiadadus <ekusiadadus@gmail.com>
2022-02-08 05:10:39 +0000

    Fix: Check a User has existed, and return 403

--- app/routers/user.py
+++ app/routers/user.py
@@ -12,13 +12,16 @@ router = APIRouter(prefix="/users", tags=["Users"])
 @router.post("", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
-    # Hash the password -user.password
-    hashed_password = utils.hash(user.password)
-    user.password = hashed_password
-    new_user = models.User(**user.dict())
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
+    try:
+        # Hash the password -user.password
+        hashed_password = utils.hash(user.password)
+        user.password = hashed_password
+        new_user = models.User(**user.dict())
+        db.add(new_user)
+        db.commit()
+        db.refresh(new_user)
+    except Exception:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"User with email {user.email} has existed.")
 
     return new_user
 
--- db/migrations/002_create_users.up.sql
+++ db/migrations/002_create_users.up.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS fastapi.users;
 CREATE TABLE users (
   id serial NOT NULL,
-  email character varying NOT NULL,
+  email character varying NOT NULL UNIQUE,
   password character varying Not Null,
   created_at timestamp with time zone NOT NULL DEFAULT NOW(),
   PRIMARY KEY (id)
