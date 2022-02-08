f92d55e0a9583dd27a49f7c9beeb9b7168a020dc
ekusiadadus <ekusiadadus@gmail.com>
2022-02-08 07:24:15 +0000

    Add: SQLAlchemy modify

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from sqlalchemy import TIMESTAMP, Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql.expression import text
 
 from .database import Base
@@ -13,6 +14,7 @@ class Post(Base):
     published = Column(Boolean, server_default="TRUE", nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text("now()"))
     user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    user = relationship("User")
 
 
 class User(Base):
