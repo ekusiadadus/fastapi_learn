5ed042b61c3f58d862faee61007be7ecc9dd99bd
ekusiadadus <ekusiadadus@gmail.com>
2022-02-08 19:41:31 +0000

    fix: corsm

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from .routers import auth, post, user, vote
 
 app = FastAPI(title="fastapi")
 
-origins = [""]
+origins = ["*"]
 app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
 app.include_router(post.router)
