2f99c950e2c5a14ee644e2802437f79506298b40
ekusiadadus <ekusiadadus@gmail.com>
2022-02-08 19:42:51 +0000

    fix: import error

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -1,6 +1,4 @@
-from sys import prefix
-
-from fastapi import APIRouter, Depends, FastAPI, HTTPException, Response, status
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 from .. import database, models, oauth2, schemas
